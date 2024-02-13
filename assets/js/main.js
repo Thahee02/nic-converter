d2bdbfe0c553b1ace7985c9c6b696d1162453ce2
HathilThahee <hathilthahee430@gmail.com>
2024-02-13 15:42:40 +0530

    built: nic-converter-app

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,46 @@
-// Write your javascript code here
+import lankaNic from "lanka-nic-2019";
+
+const formEl = document.querySelector("form");
+const nicEl = document.querySelector("input");
+const avatarEl = document.querySelector("#avatar");
+const nicNoEl = document.querySelector("#nicNo");
+const newNicNoEl = document.querySelector("#newNicNo");
+const genderEl = document.querySelector("#gender");
+const dobEl = document.querySelector("#dob");
+const nicInfoEl = document.querySelector("#nicInfo");
+
+const male = "./assets/img/male.jpg";
+const female = "./assets/img/female.jpg";
+
+const getNIC = () => {
+  if (nicEl.value == "") {
+    alert("Please enter your NIC number");
+    nicEl.focus();
+  } else {
+    if (lankaNic.validateNic(nicEl.value)) {
+      const nicInfo = lankaNic.infoNic(nicEl.value);
+      nicInfoEl.classList.remove("hidden");
+      nicNoEl.textContent = nicInfo.input.toUpperCase();
+      if (nicInfo.format == "new") {
+        newNicNoEl.textContent = nicInfo.input;
+      } else {
+        newNicNoEl.textContent = nicInfo.newFormat;
+      }
+      genderEl.textContent = nicInfo.gender;
+      if (nicInfo.gender == "Male") {
+        avatarEl.src = male;
+      } else {
+        avatarEl.src = female;
+      }
+      dobEl.textContent = nicInfo.birthday;
+    } else {
+      alert("Invalid NIC number! please enter your valid NIC number");
+    }
+  }
+};
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getNIC();
+});
+
